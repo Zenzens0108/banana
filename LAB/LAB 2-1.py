9dfd92ca2b872e40b4bd157f5ae8d233e3640af3
ParkJaesung <birth730@naver.com>
2025-03-16 21:17:45 +0900

    Add LAB folder and exercise folder

---
+++ LAB/LAB 2-1.py
@@ -0,0 +1,10 @@
+name = "박재성"
+print("나의 이름은 :", name)
+age = 26
+print("나의 나이는 :", age)
+height = 174
+print("나의 키는", height, "cm입니다.")
+sum = 10 + 20
+print("10 + 20 =", sum)
+mult = 10 * 20
+print("10 * 20 =", mult)
